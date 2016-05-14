c613b90d71aa130fe16a3d0c2ab3b083f406d5c1
Yoshi <cinkaryliss@gmail.com>
2016-05-15 01:19:24 +0900

    明日の天気も取得できるように改良

--- chapter6/get_weather1.py
+++ chapter6/get_weather1.py
@@ -3,3 +3,4 @@ api_url = 'http://weather.livedoor.com/forecast/webservice/json/v1';
 payload = {'city':'130010'}
 weather_data =requests.get(api_url, params=payload).json()
 print(weather_data['forecasts'][0]['dateLabel'] + 'の天気は' + weather_data['forecasts'][0]['telop'])
+print(weather_data['forecasts'][1]['dateLabel'] + 'の天気は' + weather_data['forecasts'][1]['telop'])
