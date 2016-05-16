4275d7783ba1d85e1a5c246a14b18e033614a908
Yoshi <cinkaryliss@gmail.com>
2016-05-16 11:41:44 +0900

    Wikipediaの情報を取得するプログラムの作成

--- chapter6/get_wiki.py
+++ chapter6/get_wiki.py
@@ -1,5 +1,7 @@
-import requests, pprint
-api_url = 'https://ja.wikipedia.org/w/api.php'
-api_params = {'format':'json', 'action':'query', 'titles':'椎名林檎', 'prop':'revisions', 'rvprop':'content'}
-wiki_data = requests.get(api_url, params=api_params).json()
-pprint.pprint(wiki_data)
+import requests
+api_base_url = 'https://ja.wikipedia.org/w/api.php'
+api_params = {'format':'xmlfm', 'action':'query', 'titles':'椎名林檎', 'prop':'revisions', 'rvprop':'content'}
+wiki_data = requests.get(api_base_url, params=api_params)
+fo = open('/Users/Yoshi/Desktop/wiki.html', 'w')
+fo.write(wiki_data.text)
+fo.close()
